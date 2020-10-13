c0f858bcb92682846d0969e9d30f519eb1aafb46
Karishnu Poddar <karishnu@gmail.com>
2020-10-13 22:35:17 +0530

    Initial Test

---
+++ ssh-test.py
@@ -0,0 +1,24 @@
+import paramiko
+from scp import SCPClient
+
+k = paramiko.RSAKey.from_private_key_file("./karishnu.pem")
+c = paramiko.SSHClient()
+
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print("connecting")
+c.connect(hostname = "35.154.44.251", username = "ubuntu", pkey = k)
+print("connected")
+
+# SCPCLient takes a paramiko transport as an argument
+scp = SCPClient(c.get_transport())
+scp.put('test.py', 'test.py')
+
+commands = [ 'python test.py' ]
+
+for command in commands:
+	print ("Executing {}".format( command ))
+	stdin , stdout, stderr = c.exec_command(command)
+	print (stdout.read())
+	print( "Errors")
+	print (stderr.read())
+c.close()
