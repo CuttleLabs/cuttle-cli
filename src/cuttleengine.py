8f89806f596621e145b76d8fec4c76e0d84b782d
Karishnu Poddar <karishnu@gmail.com>
2020-10-30 22:22:10 +0530

    Cuttle engine and plugin compile

--- src/cuttleengine.py
+++ src/cuttleengine.py
@@ -1,5 +1,59 @@
+import json
+import os
+import nbformat
 
 class CuttleEngine:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        self.config_file_name = 'cuttle.json'
 
+        config_file = open(self.config_file_name, "r")
+        config_string = config_file.read()
+
+        self.config = json.loads(config_string)
+
+    def setHomePath(self, path):
+        self.home_path = path
+
+    def getEnvironments(self):
+        return self.config['environments'].keys()
+
+    def transform(self, env_name):
+        plugin_path = os.path.join('plugin', self.config['environments'][env_name]['transformer'], 'main.py')
+        notebook_path = os.path.join(self.home_path, 'test.ipynb')
+
+        with open(notebook_path) as f:
+            nb = nbformat.read(f, as_version=4)
+
+        nb = self.envtransform(self.config, nb, 'k')
+
+        ns = {
+            'notebook': nb
+        }
+
+        with open(plugin_path) as f:
+            code = compile(f.read(), plugin_path, 'exec')
+            eval(code, ns, ns)
+
+        return ns['cli']
+
+    def envtransform(self, config, notebook, env_name):
+        cells = notebook.cells
+        cells_new = []
+
+        for cell in cells:
+            if cell['cell_type'] != 'code':
+                cells_new.append(cell)
+                continue
+            if cell['source'].find('#cuttle-environment') == -1:
+                cells_new.append(cell)
+                continue
+            if cell['source'].find('#cuttle-environment') > -1:
+                if cell['source'].find('#cuttle-environment-' + env_name + '-disable') > -1:
+                    continue
+                if cell['source'].find('#cuttle-environment-' + env_name + '\n') > -1:
+                    cells_new.append(cell)
+                    continue
+
+        notebook.cells = cells_new
+
+        return notebook
--- src/script.py
+++ src/script.py
@@ -26,36 +26,19 @@ def pass_config(f):
         
     return update_wrapper(wrapper, f)
 
-def load_config(f):
-    def wrapper(**kwargs):
-        try:
-            config_file = open(config_file_name, "r")
-            config_string = config_file.read()
-            config = json.loads(config_string)
-            kwargs['config'] = config
-            f(**kwargs)
-        except:
-            click.echo('Cuttle project not initialized.')
-            return
-
-    return wrapper
-
 class Services(click.MultiCommand):
     def list_commands(self, ctx):
-        rv = []
-        for filename in os.listdir(plugin_folder):
-            rv.append(filename)
+        cuttleengine = CuttleEngine()
+        cuttleengine.setHomePath(os.getcwd())
+        ctx.obj = cuttleengine
 
-        rv.sort()
-        return rv
+        return ctx.obj.getEnvironments()
 
     def get_command(self, ctx, name):
-        ns = {}
-        fn = os.path.join(plugin_folder, name, default_plugin_file)
-        with open(fn) as f:
-            code = compile(f.read(), fn, 'exec')
-            eval(code, ns, ns)
-        return ns['cli']
+        cuttleengine = CuttleEngine()
+        cuttleengine.setHomePath(os.getcwd())
+
+        return cuttleengine.transform(name)
 
 @click.group()
 def cli():
@@ -75,17 +58,9 @@ def create(name, transformer, config):
 
 @cli.command(cls=Services)
 @click.pass_context
-def transform(ctx):
-    try:
-        config_file = open(config_file_name, "r")
-        config_string = config_file.read()
-        config = json.loads(config_string)
-        config['home_path'] = os.getcwd()
-        ctx.obj = config
-    except Exception as e:
-        click.echo('Cuttle project not initialized correctly.')
-        click.echo(e)
-        return
+@pass_config
+def transform(ctx, config):
+    pass
 
 @cli.command()
 @click.option('--notebook', help='Notebook file name.', prompt=True, type=str, default=default_notebook_name)
